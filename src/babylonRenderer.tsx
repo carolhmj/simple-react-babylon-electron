dc0a4da4fcf43dc371dab31116c04377c2451752
Carolina Herbster <carolhmj@gmail.com>
2022-08-24 13:39:03 -0300

    Initial commit

---
+++ src/babylonRenderer.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { SceneWithSpinningBoxes } from "./sceneWithSpinningBoxes";
+
+export const BabylonRenderer = (props: any) => {
+  return (
+    <div>
+      <SceneWithSpinningBoxes />
+    </div>
+  );
+};
